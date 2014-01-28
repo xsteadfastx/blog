36648232791ff7e046332ba28cfc650b0624cd2e
Marvin Steadfast <xsteadfastx@gmail.com>
2014-01-28 22:43:40 +0100

    added CNAME file and article about dropbear

--- pelicanconf.py
+++ pelicanconf.py
@@ -25,13 +25,15 @@ CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
 
 # path-specific metadata
 EXTRA_PATH_METADATA = {
-    'extra/robots.txt': {'path': 'robots.txt'},
+    #'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/CNAME': {'path': 'CNAME'},
     }
 
 # static paths will be copied without parsing their contents
 STATIC_PATHS = [
     'pictures',
-    'extra/robots.txt',
+    #'extra/robots.txt',
+    'extra/CNAME',
     ]
 
 # custom page generated with a jinja2 template
