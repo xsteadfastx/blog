0739e2e11aa3cd547a959357b0483415faa4774f
Marvin Steadfast <marvin@xsteadfastx.org>
2015-09-15 11:47:21 +0200

    better timezone handling for new posts

--- newpost.py
+++ newpost.py
@@ -1,7 +1,10 @@
 from cookiecutter.main import cookiecutter
 from datetime import datetime
+from tzlocal import get_localzone
+import pytz
 
 
 cookiecutter(
     'cookiecutter-new-post',
-    extra_context={'date': datetime.utcnow().strftime('%Y-%m-%d %H:%M')})
+    extra_context={
+        'date': datetime.utcnow().replace(tzinfo=pytz.utc).astimezone(get_localzone()).strftime('%Y-%m-%d %H:%M')})
